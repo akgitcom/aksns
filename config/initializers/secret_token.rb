64bb817eb63ea9d9987469f1bd6a0f504f676b35
mingming <aksoft@163.com>
2013-10-03 11:23:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aksns::Application.config.secret_key_base = 'df5853e809de4d47157a6152799ba83b777e4419514db7e12590aa41bd8ac8bb826007fdb9c8eb1d8a41a0250bceda95de9e80e208928020bea9d700f486ecec'
